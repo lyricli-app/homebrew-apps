0f3790167805ed51ed9fff6046f6b4802ef30bc0
Ben Beltran <ben@nsovocal.com>
2017-11-25 13:51:00 -0600

    Update sha256 for release 0.3.0

--- lyricli-0.3.0.rb
+++ lyricli-0.3.0.rb
@@ -2,7 +2,7 @@ class Lyricli030 < Formula
   desc "Lyrics fetcher for the command-line"
   homepage "https://lyricli-app.github.io/lyricli"
   url "https://github.com/lyricli-app/lyricli/releases/download/0.3.0/lrc-0.3.0-osx.zip"
-  sha256 "a47793f88f4706c61f60df78e3ce080e5cf841965b8d61f0f6b688195f53c7cf"
+  sha256 "ee8f157a9b1d4e6356925aefab1b7ff3c31fd060999c20e750229536a83f99ee"
 
   def install
     bin.install "lrc"
